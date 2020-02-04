0474d7bc177a7b6d1f66151d3141a7e0d6d725bb
myrows <milenio204@hotmail.com>
2020-02-04 18:30:53 +0100

    [ADD] models unsplash , class detalles , recyclerview, unplashService , unsplashgenerator

--- app/src/main/java/com/salesianostriana/worldquizapp/countryDetails/DetailsActivity.java
+++ app/src/main/java/com/salesianostriana/worldquizapp/countryDetails/DetailsActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.salesianostriana.worldquizapp.R;
+import com.salesianostriana.worldquizapp.model.unsplash.Result;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements ResultFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+    }
+
+    @Override
+    public void onListFragmentInteraction(Result item) {
+
     }
 }
---
+++ app/src/main/java/com/salesianostriana/worldquizapp/countryDetails/MyResultRecyclerViewAdapter.java
@@ -0,0 +1,83 @@
+package com.salesianostriana.worldquizapp.countryDetails;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.salesianostriana.worldquizapp.R;
+import com.salesianostriana.worldquizapp.countryDetails.ResultFragment.OnListFragmentInteractionListener;
+
+import com.salesianostriana.worldquizapp.model.unsplash.Result;
+
+import java.util.List;
+
+public class MyResultRecyclerViewAdapter extends RecyclerView.Adapter<MyResultRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Result> mValues;
+    private final OnListFragmentInteractionListener mListener;
+    Context ctx;
+
+    public MyResultRecyclerViewAdapter(List<Result> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_result, parent, false);
+
+        ctx = parent.getContext();
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+
+        Glide
+                .with(ctx)
+                .load(holder.mItem.getUrls().getRegular())
+                .into(holder.imageViewResult);
+
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView imageViewResult;
+
+        public Result mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+
+            imageViewResult = view.findViewById(R.id.imageViewResult);
+
+        }
+
+    }
+}
