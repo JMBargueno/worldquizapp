1489d9fd49f47781b70018821f967e46fae5e367
Jose Manuel Barguenño López <jmbarguenolopez@gmail.com>
2020-02-04 10:54:24 +0100

    s

--- app/src/main/java/com/salesianostriana/worldquizapp/MainActivity.java
+++ app/src/main/java/com/salesianostriana/worldquizapp/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        service = ServiceGenerator
+        service = ServiceGenerator.createService()
 
 
     }
