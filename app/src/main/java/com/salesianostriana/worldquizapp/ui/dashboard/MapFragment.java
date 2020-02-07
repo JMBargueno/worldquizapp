8f4c280148747e642d995da709a85d9e64cd8a56
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2020-02-07 13:26:25 +0100

    terminados marcadores en mapa y comenzado diálogo personalizado en marker

--- app/src/main/java/com/salesianostriana/worldquizapp/ui/dashboard/MapFragment.java
+++ app/src/main/java/com/salesianostriana/worldquizapp/ui/dashboard/MapFragment.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.worldquizapp.ui.dashboard;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,13 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.salesianostriana.worldquizapp.R;
 import com.salesianostriana.worldquizapp.model.Country;
 import com.salesianostriana.worldquizapp.repository.CountryService;
 import com.salesianostriana.worldquizapp.repository.retrofit.ServiceGenerator;
@@ -32,8 +37,7 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
     private GoogleMap mMap;
     Context context;
     CountryService service;
-    List<Country> listCountries = new ArrayList<>();
-
+    List<Country> listCountries;
     public MapFragment() {
     }
 
@@ -44,7 +48,7 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
         View v = super.onCreateView(inflater, container, savedInstanceState);
         service = ServiceGenerator.createService(CountryService.class);
         new CountriesAsyncTask().execute();
-        getMapAsync(this);
+
 
         return v;
     }
@@ -55,21 +59,29 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
         mMap = googleMap;
 
         for (Country country: listCountries) {
-            Marker m = mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(country.getLatlng().get(0),country.getLatlng().get(1)))
-                    .title(country.getName())
-            );
-            m.setTag(country.getAlpha3Code());
+            if(country.getLatlng().size()==0){
+
+            }else {
+                Marker m = mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(country.getLatlng().get(0), country.getLatlng().get(1)))
+                        .title(country.getTranslations().getEs())
+                        .snippet(country.getCapital())
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                );
+
+                m.setTag(country.getAlpha3Code());
+            }
         }
 
-//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-//            @Override
-//            public boolean onMarkerClick(Marker marker) {
-//                String isoCode = marker.getTag().toString();
-//                return false;
-//            }
-//        });
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String isoCode = marker.getTag().toString();
+                return false;
+            }
+        });
 
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(LayoutInflater.from(getActivity())));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
     }
 
@@ -83,7 +95,6 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 
             try{
                 response = call.execute();
-
                 if(response.isSuccessful()){
                     return response.body();
                 }
@@ -92,20 +103,16 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
                 e.printStackTrace();
                 Toast.makeText(context, "Error de conexión", Toast.LENGTH_SHORT).show();
             }
-
-
             return null;
         }
 
         @Override
         protected void onPostExecute(List<Country> countries) {
-
             listCountries = new ArrayList<>(countries);
-
-
-
-
+            getMapAsync(MapFragment.this);
         }
 
     }
+
+
 }
