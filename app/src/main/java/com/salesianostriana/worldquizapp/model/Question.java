50de087b952989b77262919040fef85561f84e75
Jose Manuel Barguenño López <jmbarguenolopez@gmail.com>
2020-02-10 12:27:21 +0100

    Cambios en la generacion de preguntas para que en el quizz halla preguntas de varios paises y para que el titulo de las preguntas se en español

--- app/src/main/java/com/salesianostriana/worldquizapp/model/Question.java
+++ app/src/main/java/com/salesianostriana/worldquizapp/model/Question.java
@@ -57,54 +57,58 @@ public class Question {
 
     //Capital de pais
     private void typeOne() {
+        List<Country>listOfCountryForQuestion = selectThreeRandomCountry();
         //Seteamos titulo de la pregunta
-        this.setTitle("¿Cuál es la capital de " + selectedCountryList.get(0).getName() + "?");
+        this.setTitle("¿Cuál es la capital de " + listOfCountryForQuestion.get(0).getTranslations().getEs() + "?");
         //Respuesta correcta
-        this.setTrueResponse(new Response(selectedCountryList.get(0).getCapital(), true));
+        this.setTrueResponse(new Response(listOfCountryForQuestion.get(0).getCapital(), true));
         //Seteamos respuestas incorrectas
-        this.setFailResponse(new Response(selectedCountryList.get(1).getCapital(), false));
-        this.setFailResponse2(new Response(selectedCountryList.get(2).getCapital(), false));
+        this.setFailResponse(new Response(listOfCountryForQuestion.get(1).getCapital(), false));
+        this.setFailResponse2(new Response(listOfCountryForQuestion.get(2).getCapital(), false));
 
 
     }
 
     //Nombre de la moneda
     private void typeTwo() {
+        List<Country>listOfCountryForQuestion = selectThreeRandomCountry();
         //Seteamos titulo de la pregunta
-        this.setTitle("¿Cómo se llama la moneda de " + selectedCountryList.get(0).getName() + "?");
+        this.setTitle("¿Cómo se llama la moneda de " + listOfCountryForQuestion.get(0).getTranslations().getEs() + "?");
         //Respuesta correcta
-        this.setTrueResponse(new Response(selectedCountryList.get(0).getCurrencies().get(0).getName(), true));
+        this.setTrueResponse(new Response(listOfCountryForQuestion.get(0).getCurrencies().get(0).getName(), true));
         //Seteamos respuestas incorrectas
-        this.setFailResponse(new Response(selectedCountryList.get(1).getCurrencies().get(0).getName(), false));
-        this.setFailResponse2(new Response(selectedCountryList.get(2).getCurrencies().get(0).getName(), false));
+        this.setFailResponse(new Response(listOfCountryForQuestion.get(1).getCurrencies().get(0).getName(), false));
+        this.setFailResponse2(new Response(listOfCountryForQuestion.get(2).getCurrencies().get(0).getName(), false));
 
 
     }
 
     //Simbolo de la moneda
     private void typeThree() {
+        List<Country>listOfCountryForQuestion = selectThreeRandomCountry();
         //Seteamos titulo de la pregunta
-        this.setTitle("¿Cuál es el símbolo de la moneda de " + selectedCountryList.get(0).getName() + "?");
+        this.setTitle("¿Cuál es el símbolo de la moneda de " + listOfCountryForQuestion.get(0).getTranslations().getEs() + "?");
         //Respuesta correcta
-        this.setTrueResponse(new Response(selectedCountryList.get(0).getCurrencies().get(0).getSymbol(), true));
+        this.setTrueResponse(new Response(listOfCountryForQuestion.get(0).getCurrencies().get(0).getSymbol(), true));
         //Seteamos respuestas incorrectas
-        this.setFailResponse(new Response(selectedCountryList.get(1).getCurrencies().get(0).getSymbol(), false));
-        this.setFailResponse2(new Response(selectedCountryList.get(2).getCurrencies().get(0).getSymbol(), false));
+        this.setFailResponse(new Response(listOfCountryForQuestion.get(1).getCurrencies().get(0).getSymbol(), false));
+        this.setFailResponse2(new Response(listOfCountryForQuestion.get(2).getCurrencies().get(0).getSymbol(), false));
 
 
     }
 
     //Paises limitrofes
     private void typeFour() {
+        List<Country>listOfCountryForQuestion = selectThreeRandomCountry();
         List<List<String>> listCountryCodeToSelect = new ArrayList<>();
         List<String> listOfBorderCountries;
 
         //Si la lista esta vacia, le setea una vacia
-        List<String> listBorderOne = selectedCountryList.get(0).getBorders();
+        List<String> listBorderOne = listOfCountryForQuestion.get(0).getBorders();
         //Si la lista esta vacia, le setea una vacia
-        List<String> listBorderTwo = selectedCountryList.get(1).getBorders();
+        List<String> listBorderTwo = listOfCountryForQuestion.get(1).getBorders();
         //Si la lista esta vacia, le setea una vacia
-        List<String> listBorderThree = selectedCountryList.get(2).getBorders();
+        List<String> listBorderThree = listOfCountryForQuestion.get(2).getBorders();
 
 
         //Guardo las listas de iso code en una lista de lista de iso code
@@ -117,7 +121,7 @@ public class Question {
 
 
         //Seteamos titulo de la pregunta
-        this.setTitle("¿Cuál es el pais limítrofe de " + selectedCountryList.get(0).getName() + "?");
+        this.setTitle("¿Cuál es el pais limítrofe de " + listOfCountryForQuestion.get(0).getTranslations().getEs() + "?");
         //Respuesta correcta
         //Cojo el primer resultado, etc..
         this.setTrueResponse(new Response(listOfBorderCountries.get(0), true));
@@ -128,25 +132,27 @@ public class Question {
 
     //Bandera pais
     private void typeFive() {
+        List<Country>listOfCountryForQuestion = selectThreeRandomCountry();
         //Seteamos titulo de la pregunta
-        this.setTitle("¿Cuál es la bandera de " + selectedCountryList.get(0).getName() + "?");
+        this.setTitle("¿Cuál es la bandera de " + listOfCountryForQuestion.get(0).getTranslations().getEs() + "?");
         //Respuesta correcta
-        this.setTrueResponse(new Response(selectedCountryList.get(0).getFlag(), true));
+        this.setTrueResponse(new Response(listOfCountryForQuestion.get(0).getFlag(), true));
         //Seteamos respuestas incorrectas
-        this.setFailResponse(new Response(selectedCountryList.get(1).getFlag(), false));
-        this.setFailResponse2(new Response(selectedCountryList.get(2).getFlag(), false));
+        this.setFailResponse(new Response(listOfCountryForQuestion.get(1).getFlag(), false));
+        this.setFailResponse2(new Response(listOfCountryForQuestion.get(2).getFlag(), false));
 
     }
 
     //Idioma pais
     private void typeSix() {
+        List<Country>listOfCountryForQuestion = selectThreeRandomCountry();
         //Seteamos titulo de la pregunta
-        this.setTitle("¿Cuál es el idioma de " + selectedCountryList.get(0).getName() + "?");
+        this.setTitle("¿Cuál es el idioma de " + listOfCountryForQuestion.get(0).getTranslations().getEs() + "?");
         //Respuesta correcta
-        this.setTrueResponse(new Response(selectedCountryList.get(0).getLanguages().get(0).getName(), true));
+        this.setTrueResponse(new Response(listOfCountryForQuestion.get(0).getLanguages().get(0).getName(), true));
         //Seteamos respuestas incorrectas
-        this.setFailResponse(new Response(selectedCountryList.get(1).getLanguages().get(0).getName(), false));
-        this.setFailResponse2(new Response(selectedCountryList.get(2).getLanguages().get(0).getName(), false));
+        this.setFailResponse(new Response(listOfCountryForQuestion.get(1).getLanguages().get(0).getName(), false));
+        this.setFailResponse2(new Response(listOfCountryForQuestion.get(2).getLanguages().get(0).getName(), false));
 
 
     }
@@ -176,6 +182,19 @@ public class Question {
         }
 
         return searchedCountriesByIsoCode;
+    }
+
+    private List<Country> selectThreeRandomCountry(){
+        List<Country> selectedThreeRandomCountry = new ArrayList<>();
+        List <Country> copyOfSelectedCountries = new ArrayList<>(selectedCountryList) ;
+        for (int i = 0; i <3; i++){
+            int range = (copyOfSelectedCountries.size() - 1) + 1;
+            int randomNum = (int)(Math.random() * range) + 0;
+            selectedThreeRandomCountry.add(copyOfSelectedCountries.get(randomNum));
+            copyOfSelectedCountries.remove(copyOfSelectedCountries.get(randomNum));
+
+        }
+        return  selectedThreeRandomCountry;
     }
 
 
