ff3297eb27ebe2e0a3364d2db75fea577408c832
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2020-02-10 13:44:11 +0100

    Solucionado error de imagen en infowindow de Google Maps

--- app/src/main/java/com/salesianostriana/worldquizapp/ui/maps/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/salesianostriana/worldquizapp/ui/maps/CustomInfoWindowAdapter.java
@@ -1,21 +1,29 @@
 package com.salesianostriana.worldquizapp.ui.maps;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 import com.salesianostriana.worldquizapp.R;
 
-public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter, RequestListener<Drawable> {
 
     private static final String TAG = "CustomInfoWindowAdapter";
     private LayoutInflater inflater;
     ImageView ivFlag;
     TextView tvTitle,tvLang,tvCapital,tvHabs;
+    Marker currentMarker;
 
     public CustomInfoWindowAdapter(LayoutInflater inflater){
         this.inflater = inflater;
@@ -23,6 +31,7 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     @Override
     public View getInfoContents(final Marker m) {
+        currentMarker = m;
         View v = inflater.inflate(R.layout.infowindow_layout, null);
         String[] info = m.getTitle().split("#");
 
@@ -34,7 +43,8 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
         Glide.with(v.getContext())
              .load(m.getSnippet())
-                .placeholder(R.drawable.ic_flag)
+                .addListener(this)
+//                .placeholder(R.drawable.ic_flag)
              .into(ivFlag);
 
         tvTitle.setText(info[0]);
@@ -48,5 +58,19 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     @Override
     public View getInfoWindow(Marker m) {
         return null;
+    }
+
+    @Override
+    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+        return false;
+    }
+
+    @Override
+    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+        if(currentMarker.isInfoWindowShown()) {
+            currentMarker.hideInfoWindow();
+            currentMarker.showInfoWindow();
+        }
+        return false;
     }
 }
